89f0e03fa9488d42ae44f8c775f77640825f67dd
growdai1y <yhyun.kim@gmail.com>
2023-09-25 08:30:44 +0900

    update rx_merge_test

--- rx_test/src/rx_merge_test.cpp
+++ rx_test/src/rx_merge_test.cpp
@@ -1,7 +1,9 @@
 #include <gtest/gtest.h>
 
 #include <cstdio>
+#include <filesystem>
 #include <fstream>
+#include <functional>
 #include <map>
 #include <sstream>
 #include <string>
@@ -22,15 +24,8 @@ struct IconLine {
   const string str() const { return name + '\t' + icon1st + '\t' + icon2nd; }
 };
 
-std::string remove_last_whitespaces(const std::string& input) {
-  std::string result = input;
-  size_t lastWhitespace = result.find_last_of(" \t\n\r\f\v");
-
-  if (lastWhitespace != std::string::npos) {
-    result.erase(lastWhitespace);
-  }
-
-  return result;
+void remove_last_whitespaces(std::string& str) {
+  while (!str.empty() && isspace(str.back())) str.pop_back();
 }
 
 IconLine to_icon_line(const string& line) {
@@ -52,6 +47,18 @@ IconLine to_icon_line(const string& line) {
     is_launcher ? IconLine::Type::Launcher : IconLine::Type::Source
   };
   // clang-format on
+}
+
+IconLine icon_line_with_fullpath(const IconLine& icon,
+                                 const map<string, string>& paths) {
+  auto copy = icon;
+  auto itr = paths.find(icon.icon1st);
+  if (itr != paths.end()) copy.icon1st = itr->second;
+
+  itr = paths.find(icon.icon2nd);
+  if (itr != paths.end()) copy.icon2nd = itr->second;
+
+  return copy;
 }
 
 struct IconMap {
@@ -110,7 +117,7 @@ rxcpp::observable<string> read_lines(const string& filepath) {
     string line;
     while (getline(file, line)) {
       if (line.empty()) continue;
-      
+
       s.on_next(line);
     }
 
@@ -118,34 +125,101 @@ rxcpp::observable<string> read_lines(const string& filepath) {
   });
 }
 
-TEST(rxcpp, read_source_n_launcher_files) {
-  auto sourceLines = read_lines("../data/source_icon/Source/Icon/logolist.info")
-                         .skip(1)
-                         .map(to_icon_line);
+TEST(rxcpp, read_lines) {
+  auto lines =
+      read_lines("../data/source_icon/Source/Icon/logolist.info").skip(1);
 
-  auto launcherLines =
-      read_lines("../data/source_icon/Launcher/Icon/logolist.info")
-          .skip(1)
-          .map(to_icon_line);
+  vector<string> lines_read;
+  lines.subscribe([&](auto line) { lines_read.push_back(line); });
 
-  IconMap iconMap;
-  sourceLines.merge(launcherLines)
-      .reduce(IconMap(),
-              [](auto container, auto iconLine) {
-                container.update(iconLine);
-                return container;
-              })
-      .subscribe([&](auto i) { iconMap = i; });
+  ASSERT_EQ(lines_read[0], "Roku	source_icon_roku.png");
+  ASSERT_EQ(lines_read[1], "Game	source_icon_game.png");
+  ASSERT_EQ(lines_read[2], "XBox One	source_icon_xboxone.png");
+}
+
+TEST(rxcpp, to_icon_line) {
+  const auto str = "Roku	source_icon_roku.png";
+  auto line = to_icon_line(str);
+
+  ASSERT_EQ(line.name, "Roku");
+  ASSERT_EQ(line.icon1st, "source_icon_roku.png");
+  ASSERT_EQ(line.icon2nd, "");
+}
+
+TEST(rxcpp, icon_map) {
+  auto rokuSource =
+      IconLine{"Roku", "source_icon_roku.png", "", IconLine::Type::Source};
+  auto rokuLauncher = IconLine{"Roku", "launcher_f_roku.png",
+                               "launcher_n_roku.png", IconLine::Type::Launcher};
+
+  auto iconMap = IconMap();
+  iconMap.update(rokuSource);
+  iconMap.update(rokuLauncher);
 
   auto roku = iconMap.get_icon("Roku");
   ASSERT_EQ(roku.name, "Roku");
   ASSERT_EQ(roku.source_icon, "source_icon_roku.png");
   ASSERT_EQ(roku.launcher_f_icon, "launcher_f_roku.png");
   ASSERT_EQ(roku.launcher_n_icon, "launcher_n_roku.png");
+}
+
+auto build_path_map(const string& dirpath) {
+  map<string, string> pathMap;
+  for (const auto& entry : filesystem::directory_iterator(dirpath)) {
+    pathMap[entry.path().filename().string()] =
+        entry.path().parent_path().string() + '/' +
+        entry.path().filename().string();
+  }
+
+  return pathMap;
+}
+
+TEST(rxcpp, build_absolute_filepaths) {
+  auto pathMap = build_path_map("../data/source_icon/Source/Icon");
+  auto itr = pathMap.find("source_icon_roku.png");
+
+  ASSERT_EQ(itr->second,
+            "../data/source_icon/Source/Icon/source_icon_roku.png");
+}
+
+auto read_icon_lines(const string& icondir, const string& logofile) {
+  auto pathMap = build_path_map(icondir);
+  auto lines =
+      read_lines(logofile)
+          .skip(1)
+          .map(to_icon_line)
+          .map(bind(icon_line_with_fullpath, placeholders::_1, pathMap));
+  return lines;
+}
+
+TEST(rxcpp, read_source_n_launcher_files) {
+  auto iconMap =
+      read_icon_lines("../data/source_icon/Source/Icon",
+                      "../data/source_icon/Source/Icon/logolist.info")
+          .merge(read_icon_lines(
+              "../data/source_icon/Launcher/Icon/2K",
+              "../data/source_icon/Launcher/Icon/logolist.info"))
+          .reduce(IconMap(),
+                  [](auto container, auto iconLine) {
+                    container.update(iconLine);
+                    return container;
+                  })
+          .as_blocking()
+          .last();
+
+  auto roku = iconMap.get_icon("Roku");
+  ASSERT_EQ(roku.name, "Roku");
+  ASSERT_EQ(roku.source_icon,
+            "../data/source_icon/Source/Icon/source_icon_roku.png");
+  ASSERT_EQ(roku.launcher_f_icon,
+            "../data/source_icon/Launcher/Icon/2K/launcher_f_roku.png");
+  ASSERT_EQ(roku.launcher_n_icon,
+            "../data/source_icon/Launcher/Icon/2K/launcher_n_roku.png");
 
   auto game = iconMap.get_icon("Game");
   ASSERT_EQ(game.name, "Game");
-  ASSERT_EQ(game.source_icon, "source_icon_game.png");
+  ASSERT_EQ(game.source_icon,
+            "../data/source_icon/Source/Icon/source_icon_game.png");
   ASSERT_EQ(game.launcher_f_icon, "");
   ASSERT_EQ(game.launcher_n_icon, "");
 }
